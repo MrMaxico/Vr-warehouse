29d532dc8b80cb3f4803824ab0522ecd221305cc
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2023-02-17 17:03:17 +0100

    load level

---
+++ VR ikea/Assets/Arthurs code/WarehouseManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warehuis : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadLevel(int num) {
+        if(WarehouseManager)
+    }
+}
---
+++ VR ikea/Assets/Arthurs code/Warehuis.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WarehouseManager : MonoBehaviour
+{
+    public int levelCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel(int num) {
+        if(levelCounter == num) {
+            SceneManager.LoadScene(num);
+        }
+    }
+}
