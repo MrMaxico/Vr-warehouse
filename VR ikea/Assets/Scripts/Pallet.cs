51b21f917980d51f96a92c4228d3d6cae7c52f9a
LusseveldNiek <97088465@st.deltion.nl>
2023-02-22 15:34:18 +0100

    pallet

--- VR ikea/Assets/Scripts/Pallet.cs
+++ VR ikea/Assets/Scripts/Pallet.cs
@@ -5,15 +5,45 @@ using UnityEngine;
 public class Pallet : MonoBehaviour
 {
     public GameObject[] palletItems;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Transform[] bigItems;
+    public Transform[] mediumItems;
+    public Transform[] smallItems;
+    
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.GetComponent<ItemData>().size == "Small")
+        {
+            for (int i = 0; i < smallItems.Length; i++)
+            {
+                if(smallItems[i] == null)
+                {
+                    other.transform.position = smallItems[i].position;
+                }
+            }
+        }
+
+        if (other.GetComponent<ItemData>().size == "Medium")
+        {
+            for (int i = 0; i < mediumItems.Length; i++)
+            {
+                if (mediumItems[i] == null)
+                {
+                    other.transform.position = mediumItems[i].position;
+                }
+            }
+        }
+
+        if (other.GetComponent<ItemData>().size == "Big")
+        {
+            for (int i = 0; i < bigItems.Length; i++)
+            {
+                if (bigItems[i] == null)
+                {
+                    other.transform.position = bigItems[i].position;
+                }
+            }
+        }
     }
 }
