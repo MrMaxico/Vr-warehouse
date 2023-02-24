41983a6190eb65f07bbb56a6d4c4ebc7130a09ab
LusseveldNiek <97088465@st.deltion.nl>
2023-02-24 14:12:57 +0100

    update orderListManager

--- VR ikea/Assets/Scripts/OrderListManager.cs
+++ VR ikea/Assets/Scripts/OrderListManager.cs
@@ -11,17 +11,16 @@ public class OrderListManager : MonoBehaviour
     public bool isCompleted;
     public GameObject warehouseManager;
     public int itemValue;
+    public Text[] itemNames;
 
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < itemsOrder.Length; i++)
         {
-            //itemNames[i] = itemsOrder[i].GetComponent<ItemData>().itemData.itemName; //kijkt in scriptableObject voor naam item
+            itemNames[i].text = itemsOrder[i].GetComponent<ItemData>().itemName; //kijkt in scriptableObject voor naam item
         }
     }
-
-    
     void Update()
     {
       
@@ -37,12 +36,13 @@ public class OrderListManager : MonoBehaviour
             correctItems = itemsOrder.Intersect(other.GetComponent<Pallet>().palletItems).ToArray<GameObject>();
             if(correctItems.Length == itemsOrder.Length)
             {
-                    //alle items zitten op de pallet en gaan in de bus
+                print("allItemsCorrect");
+                //alle items zitten op de pallet en gaan in de bus
             }
                 
             else
             {
-                for (int i = 0; i < other.GetComponent<Pallet>().palletItems.Length; i++)
+                for (int i = 0; i < other.GetComponent<Pallet>().palletItems.Count; i++)
                 {
                     itemsOrder.Intersect(other.GetComponent<Pallet>().palletItems).Any();
                     
@@ -53,7 +53,6 @@ public class OrderListManager : MonoBehaviour
                         correctItems[g].GetComponent<Renderer>().material.color = Color.green;
                         // de correcte items worden groen maar de order is niet compleet
                     }
-                    
                 }
             }
         }
--- VR ikea/Assets/Scripts/Pallet.cs
+++ VR ikea/Assets/Scripts/Pallet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Pallet : MonoBehaviour
 {
-    public GameObject[] palletItems;
+    public List<GameObject> palletItems;
 
     public Transform[] bigItems;
     public Transform[] mediumItems;
@@ -20,6 +20,7 @@ public class Pallet : MonoBehaviour
                 if(smallItems[i] == null)
                 {
                     other.transform.position = smallItems[i].position;
+                    palletItems.Add(other.gameObject);
                 }
             }
         }
@@ -31,17 +32,20 @@ public class Pallet : MonoBehaviour
                 if (mediumItems[i] == null)
                 {
                     other.transform.position = mediumItems[i].position;
+                    palletItems.Add(other.gameObject);
                 }
             }
         }
 
         if (other.GetComponent<ItemData>().size == "Big")
         {
+            print("hoi");
             for (int i = 0; i < bigItems.Length; i++)
             {
                 if (bigItems[i] == null)
                 {
                     other.transform.position = bigItems[i].position;
+                    palletItems.Add(other.gameObject);
                 }
             }
         }
