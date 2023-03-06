5a83d13309ca8e8393e7be2ebe8fac83f6b61a2d
LusseveldNiek <97088465@st.deltion.nl>
2023-03-06 11:06:17 +0100

    pallet

--- VR ikea/Assets/Scripts/Pallet.cs
+++ VR ikea/Assets/Scripts/Pallet.cs
@@ -9,45 +9,47 @@ public class Pallet : MonoBehaviour
     public Transform[] bigItems;
     public Transform[] mediumItems;
     public Transform[] smallItems;
+
+    private int bigItemCounter;
+    private int mediumItemCounter;
+    private int smallItemCounter;
     
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<ItemData>().size == "Small")
         {
-            for (int i = 0; i < smallItems.Length; i++)
+            if(smallItemCounter < smallItems.Length)
             {
-                if(smallItems[i] == null)
-                {
-                    other.transform.position = smallItems[i].position;
-                    palletItems.Add(other.gameObject);
-                }
+                other.transform.position = smallItems[smallItemCounter].position;
+                smallItemCounter++;
+                palletItems.Add(other.gameObject);
             }
         }
 
         if (other.GetComponent<ItemData>().size == "Medium")
         {
-            for (int i = 0; i < mediumItems.Length; i++)
+            print("nee");
+            if (mediumItemCounter < mediumItems.Length)
             {
-                if (mediumItems[i] == null)
-                {
-                    other.transform.position = mediumItems[i].position;
-                    palletItems.Add(other.gameObject);
-                }
-            }
+                print("ja");
+                other.transform.position = mediumItems[mediumItemCounter].position;
+                mediumItemCounter++;
+                palletItems.Add(other.gameObject);
+            } 
+            
         }
 
         if (other.GetComponent<ItemData>().size == "Big")
         {
-            print("hoi");
-            for (int i = 0; i < bigItems.Length; i++)
+            if (bigItemCounter < bigItems.Length)
             {
-                if (bigItems[i] == null)
-                {
-                    other.transform.position = bigItems[i].position;
-                    palletItems.Add(other.gameObject);
-                }
+                other.transform.position = bigItems[bigItemCounter].position;
+                bigItemCounter++;
+                palletItems.Add(other.gameObject);
             }
+                
+            
         }
     }
 }
