8e33b62d38aab0f1e6a195f5c58caed0fa945884
LusseveldNiek <97088465@st.deltion.nl>
2023-03-06 12:58:59 +0100

    items

--- VR ikea/Assets/Scripts/Pallet.cs
+++ VR ikea/Assets/Scripts/Pallet.cs
@@ -7,34 +7,37 @@ public class Pallet : MonoBehaviour
     public List<GameObject> palletItems;
 
     public Transform[] bigItems;
+    private bool hasToBeFilled; //zorgt ervoor dat 1 item niet alles in de pallet vult, een glitch
     
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.GetComponent<ItemData>().size == "Medium")
         {
+            hasToBeFilled = true;
             for (int i = 0; i < bigItems.Length; i++)
             {
-                if (bigItems[i].gameObject.GetComponent<PalletPosition>().isFull == false)
+                foreach (Transform child in bigItems[i])
                 {
-                    foreach(Transform child in bigItems[i])
+                    if (child.gameObject.GetComponent<PalletPosition>().isFull == false && hasToBeFilled)
                     {
-                        if(child.gameObject.GetComponent<PalletPosition>().isFull == false)
-                        {
-                            other.transform.position = child.position;
-                            palletItems.Add(other.gameObject);
-                            child.gameObject.GetComponent<PalletPosition>().isFull = true;
-                            bigItems[i].gameObject.GetComponent<PalletPosition>().isFull = true;
-                        }
+                        other.transform.position = child.position;
+                        palletItems.Add(other.gameObject);
+                        child.gameObject.GetComponent<PalletPosition>().isFull = true;
+                        bigItems[i].gameObject.GetComponent<PalletPosition>().isFull = true;
+                        hasToBeFilled = false;
                     }
                 }
+
+
 
             }
         }
 
+        //als er een grote item op de pallet wordt gelegd
         if (other.GetComponent<ItemData>().size == "Big")
         {
+            hasToBeFilled = true;
             for (int i = 0; i < bigItems.Length; i++)
             {
                 if (bigItems[i].gameObject.GetComponent<PalletPosition>().isFull == false)
@@ -42,10 +45,9 @@ public class Pallet : MonoBehaviour
                     other.transform.position = bigItems[i].position;
                     palletItems.Add(other.gameObject);
                     bigItems[i].gameObject.GetComponent<PalletPosition>().isFull = true;
+                    hasToBeFilled = false;
                 }
             }
-                
-       
         }
     }
 }
