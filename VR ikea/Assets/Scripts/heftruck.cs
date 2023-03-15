bde8489648b120951353c3e7ff405e257b4f26ff
LusseveldNiek <97088465@st.deltion.nl>
2023-03-15 13:03:18 +0100

    heftruck script

---
+++ VR ikea/Assets/Scripts/heftruck.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heftruck : MonoBehaviour
+{
+    public GameObject playerOrigin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //playerOrigin.GetComponent<XROrigin>()
+    }
+
+    void Gas()
+    {
+
+    }
+
+    void Brake()
+    {
+
+    }
+
+    void Forks()
+    {
+
+    }
+
+
+}
