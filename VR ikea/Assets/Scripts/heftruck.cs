f4b45750a88d553914a2c08e97cdb2d60e5768f4
LusseveldNiek <97088465@st.deltion.nl>
2023-03-15 14:46:46 +0100

    heftruck

--- VR ikea/Assets/Scripts/heftruck.cs
+++ VR ikea/Assets/Scripts/heftruck.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class heftruck : MonoBehaviour
 {
     public GameObject playerOrigin;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private RaycastHit hit;
+    private bool inHeftruck;
+    
     void Update()
     {
-        //playerOrigin.GetComponent<XROrigin>()
+        Physics.Raycast(playerOrigin.transform.position, playerOrigin.transform.forward, out hit, 10);
+        if (hit.transform.gameObject.tag == "heftruck" && Input.GetKeyDown(KeyCode.H)) // speler kijkt naar heftruck en klikt op "h" om in te stappen
+        {
+            inHeftruck = true;
+            playerOrigin.transform.position = hit.transform.position;
+            playerOrigin.transform.rotation = hit.transform.rotation;
+        }
+
+        if (Input.GetKeyDown(KeyCode.H) && inHeftruck)
+        {
+            inHeftruck = false;
+            playerOrigin.transform.position = transform.position - new Vector3(2, 0, 0); // spawn speler naast heftruck, de speler stapt uit
+        }
+
+        if(inHeftruck)
+        {
+            Gas();
+            Brake();
+            Forks();
+        }
     }
 
     void Gas()
