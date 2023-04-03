a4f20a2dc042e4dfcdd555533446d5241e8669d8
LusseveldNiek <97088465@st.deltion.nl>
2023-04-03 11:12:26 +0200

    pallet

---
+++ VR ikea/Assets/Scripts/palletWagen.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class palletWagen : MonoBehaviour
+{
+    public Transform topJoystickGas;
+    public float forwardBackwardTiltGas = 0;
+    private bool holdingStick;
+    private float speed;
+    public GameObject wagen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (holdingStick)
+        {
+            wagen.transform.Translate(transform.forward * speed);
+            forwardBackwardTiltGas = topJoystickGas.rotation.eulerAngles.x;
+            if (forwardBackwardTiltGas < 355 && forwardBackwardTiltGas > 290)
+            {
+                forwardBackwardTiltGas = Mathf.Abs(forwardBackwardTiltGas - 360);
+                Debug.Log("Backward" + forwardBackwardTiltGas);
+                speed = (-forwardBackwardTiltGas / 1400) / 2;
+            }
+
+            else if (forwardBackwardTiltGas > 5 && forwardBackwardTiltGas < 74)
+            {
+                Debug.Log("Forward" + forwardBackwardTiltGas);
+                speed = (forwardBackwardTiltGas / 1400) / 2;
+
+            }
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == ("playerRightHand"))
+        {
+            print("wauw");
+            transform.LookAt(other.transform.position, transform.up);
+            holdingStick = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == ("playerRightHand"))
+        {
+            holdingStick = false;
+        }
+    }
+}
